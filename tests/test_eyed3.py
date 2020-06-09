970ed0a9e53cb953c035fc2463b68b384b8c9cb8
igrek51 <igrek51.dev@gmail.com>
2020-06-09 22:52:56 +0200

    add test results to gitignore

--- tests/test_eyed3.py
+++ tests/test_eyed3.py
@@ -5,9 +5,9 @@ from pydub import AudioSegment
 
 def test_export_id3_tags():
     song = AudioSegment.from_mp3("./tests/tubular.mp3")
-    song.export("./tests/tubular_tags.mp3", format="mp3")
+    song.export("./tests/result_tubular_tags.mp3", format="mp3")
 
-    audiofile = eyed3.load("./tests/tubular_tags.mp3")
+    audiofile = eyed3.load("./tests/result_tubular_tags.mp3")
     audiofile.tag.artist = "Artistążół"
     audiofile.tag.title = "I'm a pickle ążśółć too long title to be displayed"
 
--- tests/test_pydub.py
+++ tests/test_pydub.py
@@ -8,9 +8,9 @@ def test_volume_normalize():
     assert 3 < gain < 4
 
     normalized_song = song.apply_gain(gain)
-    normalized_song.export("./tests/tubular_normalized.mp3", format="mp3")
+    normalized_song.export("./tests/result_tubular_normalized.mp3", format="mp3")
 
-    normalized_song = AudioSegment.from_mp3("./tests/tubular_normalized.mp3")
+    normalized_song = AudioSegment.from_mp3("./tests/result_tubular_normalized.mp3")
     assert -0.6 < normalized_song.max_dBFS < 0.6
 
 
@@ -38,8 +38,8 @@ def test_trim_down_the_silence():
     duration = len(song)
     trimmed_song = song[start_trim:duration - end_trim]
 
-    trimmed_song.export("./tests/tubular_trimmed.mp3", format="mp3")
-    trimmed_song = AudioSegment.from_mp3("./tests/tubular_trimmed.mp3")
+    trimmed_song.export("./tests/result_tubular_trimmed.mp3", format="mp3")
+    trimmed_song = AudioSegment.from_mp3("./tests/result_tubular_trimmed.mp3")
     duration_s = len(trimmed_song) / 1000
     assert 76 < duration_s < 77
 
@@ -47,4 +47,4 @@ def test_trim_down_the_silence():
 def test_fade_out():
     song = AudioSegment.from_mp3("./tests/tubular.mp3")
     faded_song = song.fade_in(500).fade_out(2000)
-    faded_song.export("./tests/tubular_faded.mp3", format="mp3")
+    faded_song.export("./tests/result_tubular_faded.mp3", format="mp3")
